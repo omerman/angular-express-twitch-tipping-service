58111be2f934ce8cb4e32e34e733c070766bc63c
Omer Prizner <omer.prizner@gmail.com>
2017-03-27 22:32:52 +0300

    testing something regarding scss

--- bin/get-webpack-config.js
+++ bin/get-webpack-config.js
@@ -85,13 +85,20 @@ module.exports = ({ isProd = false, isWebpackDevServer = false } = {}) => ({
         ]
       }, {
           test: /\.scss$/,
-          use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
-          }]
+          use: new ExtractTextPlugin({
+            filename: 'bundle.css',
+            disable: isWebpackDevServer
+          }).extract({
+            use: [
+              {
+                  loader: "style-loader" // creates style nodes from JS strings
+              }, {
+                  loader: "css-loader" // translates CSS into CommonJS
+              }, {
+                  loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+          })
       }, {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
